cadeac5c606911dedc3ffa4a17a4b2c39762f931
Sayujya Malkan <sayujyamalkan@gamil.com>
2022-06-16 23:12:22 +0530

    fetched all pokemon data

--- src/App.js
+++ src/App.js
@@ -2,49 +2,69 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import PokemonGrid from './components/PokemonGrid';
 
 
 function App() {
 
   const [searchQuery, setSearchQuery] = useState("");
-
+  const [loading, setLoading] = useState(true)
   const [pokemonList, setPokemonList] = useState([]);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+  const [nextUrl, setNextUrl] = useState()
+  const [prevUrl, setPrevUrl] = useState()
+  
+
+  const getPokemonArray = async (res) => {
+    res.map(async(pokemon) => {
+      const result = await axios.get(pokemon.url)
+      // console.log(result.data)
+      setPokemonList(currState => {
+        currState = [...currState, result]
+        return currState;
+      })
+    })
+  }
+
+  const fetchAllPokemon = async () => {
+    setLoading(true)
+    const res = await axios.get(url)
+    // console.log(res.data.results)
+    setNextUrl(res.data.next)
+    setPrevUrl(res.data.previous)
+    getPokemonArray(res.data.results)
+
+    setLoading(false)
+  } 
+
+
+  useEffect(() => {
+    fetchAllPokemon()
+  }, [url])
+
+
 
   const handleInputChange = (e) => {
     e.preventDefault();
     setSearchQuery(e.target.value);
+    // console.log(e.target.value);
   }
 
-const fetchedData = () => {
-  const req = axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => setPokemonList(response.data.results))
-  console.log(pokemonList)
-}
-
-useEffect(() => {
-  fetchedData()
-}, [])
 
 
 
 
-  return (
-    <div className="App">
-      <h1>pokemon</h1>
 
-      {/* This will contain the input for filtering the pokemon cards */}
-      <nav>
-        <input type='text' id='search' placeholder='Search...' onChange={handleInputChange}/>
-      </nav>
-        {
-          pokemonList.map((item) => 
-          <div key={item.name} className='card'>
-            <h1 className='name-header'>{item.name}</h1>
-          </div>
-          
-          )
-        }
-    </div>
-  );
+return (
+  <div className="App">
+    <h1>pokemon</h1>
+    {/* This will contain the input for filtering the pokemon cards */}
+    <nav>
+      <input type='text' id='search' placeholder='Search...' onChange={handleInputChange}/>
+    </nav>
+  </div>
+);
+
 }
 
 export default App;
